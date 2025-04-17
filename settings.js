bbca14bc40c51a36779c7604334e079430ce3154
SKIDDYcoder <getrudakakusa924@gmail.com>
2025-04-17 15:09:11 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "255619712382"
+global.ownernomer = "255741927849 "
 global.ownername = "ElitePro"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
 
 
-global.ownernumber = '255619712382'  //creator number
+global.ownernumber = 255741927849 '  //creator number
 global.ownername = 'ElitePro' //owner name
 global.botname = 'ELITE-PRO-V1' //name of the bot
 
